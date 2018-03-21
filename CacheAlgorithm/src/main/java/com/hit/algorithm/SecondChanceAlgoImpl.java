690a0bb675b8b6c16cad7b2087c895936d539255
malknaor <malknaor@gmail.com>
2018-03-21 18:00:18 +0200

    With second-chance

--- CacheAlgorithm/src/main/java/com/hit/algorithm/SecondChanceAlgoImpl.java
+++ CacheAlgorithm/src/main/java/com/hit/algorithm/SecondChanceAlgoImpl.java
@@ -1,64 +1,114 @@
 package com.hit.algorithm;
 
-import java.util.Queue;
+import java.util.*;
 
 public class SecondChanceAlgoImpl<K, V> extends AbstractAlgoCache<K, V> implements IAlgoCache<K, V> {
 
+    Queue<Complex<K, V>> cacheQueue;
 
     public SecondChanceAlgoImpl(int capacity) {
         super(capacity);
+        cacheQueue = new LinkedList<>();
     }
 
     @Override
     public V getElement(K key) {
-        return null;
+        V retVal = null;
+
+        for (Complex<K, V> complex : cacheQueue) {
+            if (complex.key == key) {
+                retVal = complex.value;
+                break;
+            }
+        }
+
+        return retVal;
     }
 
     @Override
     public V putElement(K key, V value) {
-        return null;
+        V retVal = null;
+        boolean checkIfExist = ifKeyExist(key);
+
+        if (checkIfExist) {
+            for (Complex<K, V> complex: cacheQueue) {
+                if (complex.key == key){
+                    complex.setRefBit(true);
+                    retVal = complex.value;
+                    break;
+                }
+            }
+        } else if (cacheQueue.size() < capacity) {
+            Complex<K, V> tempComplex = new Complex<>(key, value);
+            retVal = tempComplex.value;
+            cacheQueue.add(tempComplex);
+        }
+
+        return retVal;
     }
 
     @Override
     public void removeElement(K key) {
+        Complex<K, V> toRemove = null;
 
+        for (Complex<K, V> complex : cacheQueue){
+            if (complex.key == key) {
+                toRemove = complex;
+                cacheQueue.remove(toRemove);
+                break;
+            }
+        }
     }
 
-    class Complex<K, V> {
+    private boolean ifKeyExist(K key) {
+        boolean exist = true;
+
+        for (Complex<K, V> complex : cacheQueue){
+            if (complex.key == key)
+                exist = false;
+        }
+
+        return exist;
+    }
+
+    private V findPageToReplace(K key, V value) {
+        V retVal = null;
+        Complex<K, V> tempComplex = cacheQueue.poll();
+
+        if (tempComplex != null) {
+            if (tempComplex.isRefBit()) {
+                tempComplex.setRefBit(false);
+                retVal = findPageToReplace(key, value);
+                cacheQueue.add(tempComplex);
+            } else {
+                retVal = tempComplex.value;
+            }
+        }
+
+        return retVal;
+    }
+
+    private class Complex<K, V> {
         K key;
         V value;
-        boolean validBit;
-        boolean referenceBit;
+        boolean refBit;
 
         public Complex(K key, V value) {
             this.key = key;
             this.value = value;
-            validBit = true;
-            referenceBit = false;
-        }
-
-        public K getKey() {
-            return key;
+            refBit = false;
         }
 
         public V getValue() {
             return value;
         }
 
-        public boolean isValidBit() {
-            return validBit;
+        public boolean isRefBit() {
+            return refBit;
         }
 
-        public void setValidBit(boolean validBit) {
-            this.validBit = validBit;
-        }
-
-        public boolean isReferenceBit() {
-            return referenceBit;
-        }
-
-        public void setReferenceBit(boolean referenceBit) {
-            this.referenceBit = referenceBit;
+        public void setRefBit(boolean refBit) {
+            this.refBit = refBit;
         }
     }
 }
